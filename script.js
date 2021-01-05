26e48a141358243d4421cd94f77ac8f9009e408f
Monzer Mohammad <72494044+DevMonzer@users.noreply.github.com>
2021-01-05 10:05:48 +0200

    added the openModal function

--- script.js
+++ script.js
@@ -17,3 +17,6 @@ const openModal = function () {
 
 for (let i = 0; i < btnsOpenModal.length; i++)
   btnsOpenModal[i].addEventListener('click', openModal);
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
