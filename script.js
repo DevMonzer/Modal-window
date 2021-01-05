f0c1529862c8a5743a138f7e7dff971cbd161d4d
Monzer Mohammad <72494044+DevMonzer@users.noreply.github.com>
2021-01-05 09:40:22 +0200

    added the closeModal function

--- script.js
+++ script.js
@@ -4,3 +4,8 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
